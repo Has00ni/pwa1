66e0cfa036ee53e38945ccb67775af20c572bbef
Hasooni <Hussein98@live.nl>
2025-06-22 04:02:59 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+// Elke build unieke cache-naam om verouderde bestanden te voorkomen
+const CACHE_NAME = 'pwa-cache-' + new Date().getTime();
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Oude caches opruimen
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
